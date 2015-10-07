c55d8ddfccd157cc9395a342c78cd7fde2daa0b7
alejandroSaura <alejandro.saura.villanueva@gmail.com>
2015-10-08 07:23:15 +0800

    counts vertex iteratively through nodes

--- FBXvertexCounter/FBXvertexCounter/FBXvertexCounter.py
+++ FBXvertexCounter/FBXvertexCounter/FBXvertexCounter.py
@@ -1,1 +1,40 @@
-from fbx import *
+﻿import Image
+import sys
+import fbx
+
+filepath = 'cubeman/cubeMan.fbx'
+
+manager = fbx.FbxManager.Create()
+importer = fbx.FbxImporter.Create(manager, 'myImporter')
+
+status = importer.Initialize(filepath)
+if status == False :
+  print 'fbx initialization failed'
+  print 'Error: %s' % importer.GetLastErrorString()
+  sys.exit()
+
+scene = fbx.FbxScene.Create( manager, 'myScene')
+importer.Import(scene)
+importer.Destroy()
+
+root = scene.GetRootNode()
+
+vertexCount = 0
+
+def countVertex (node) :
+    global vertexCount
+    mesh = node.GetMesh()
+    if(mesh != None) :
+      vertexCount = vertexCount + mesh.GetControlPointsCount()
+    childNumber = node.GetChildCount()
+    count = list(range(childNumber))
+    for i in count:
+        countVertex(node.GetChild(i))
+
+
+countVertex(root)
+print'number of vertex: %s' % vertexCount
+
+
+
+
